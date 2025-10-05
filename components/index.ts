d8daf5bfd30743f9cd7c5f8e1a1c9de64ed13f1d
Shrysxs <shreyashborade2@gmail.com>
2025-10-05 15:24:05 +0530

    fix: remove unexported useChart from index

--- components/index.ts
+++ components/index.ts
@@ -6,5 +6,5 @@ export { ReportsSection } from "./ReportsSection"
 
 // UI Components
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./ui/card"
-export { ChartContainer, ChartTooltip, ChartTooltipContent, useChart } from "./ui/chart"
+export { ChartContainer, ChartTooltip, ChartTooltipContent } from "./ui/chart"
 export type { ChartConfig } from "./ui/chart"
